4129c825bd268c4d9ca57ea562696f548b9da7d0
User <User@DESKTOP-CU12B7G>
2022-07-06 21:34:29 -0300

    Adicionar arquivos de projeto.

---
+++ trabalho djalma/Prova Djalma4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho_djalma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string j = "S";
+
+            do
+            {
+
+                Console.Clear();
+                for (int i = 1; i <= 50; i++)
+                {
+                    Console.WriteLine(i);
+
+                }
+                Console.WriteLine();
+
+                Console.Write("Deseja usar novamente? (Sim/Nao): ");
+
+                j = Console.ReadLine();
+
+                if (j != "N" && j != "n" && j != "S" && j != "s")
+                {
+
+                    Console.WriteLine("ERROR!");
+                    Console.ReadKey();
+                    Environment.Exit(0);
+
+                }
+
+
+            }
+            while (j == "S" || j == "s");
+
+            Console.WriteLine("OBRIGADA POR USAR VOLTE SEMPRE :)");
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
